f71b6e1afa9e8727f47ccb757374eb9e951776d1
Mateo Nieto <matheo.developer@gmail.com>
2025-10-27 16:03:21 -0500

    added: structure of the project

---
+++ src/shared/navigators/StackNavigator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+type Props = {
+  initialRouteName: string;
+  routes: any[];
+  screenOptions: any;
+};
+export const StackNavigator: React.FC<Props> = ({ routes, ...props }) => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator {...props}>
+      {routes?.map((item, key) => (
+        <Stack.Screen
+          key={key}
+          name={item.name}
+          component={item.component}
+          options={item.options}
+        />
+      ))}
+    </Stack.Navigator>
+  );
+};
