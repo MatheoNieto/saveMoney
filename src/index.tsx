b172109cf33f775d50e4ea0a2f6b06a5b3933ac6
Mateo Nieto <matheo.developer@gmail.com>
2025-10-29 22:07:22 -0500

    added: theme save money

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,24 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {Provider as StoreProvider} from 'react-redux';
 import {persistor, store} from '@store/store';
 import {NavigationContainer} from '@react-navigation/native';
+import ThemeProvider from '@theme/ThemeProvider';
+import {
+  initialWindowMetrics,
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
 
 export const Application = () => {
   return (
-    <StoreProvider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </PersistGate>
-    </StoreProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <StoreProvider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <ThemeProvider>
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
+          </ThemeProvider>
+        </PersistGate>
+      </StoreProvider>
+    </SafeAreaProvider>
   );
 };
---
+++ src/shared/theme/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {ThemeProvider as RestyleProvider} from '@shopify/restyle';
+
+import lightTheme from './lightTheme';
+import darkTheme from './darkTheme';
+import {useIsDarkMode} from './hook';
+
+type Props = {
+  children: React.ReactElement;
+};
+const ThemeProvider: React.FC<Props> = ({children}) => {
+  const isDarkMode = useIsDarkMode();
+  return (
+    <RestyleProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      {children}
+    </RestyleProvider>
+  );
+};
+
+export default ThemeProvider;
